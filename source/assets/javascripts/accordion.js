f2dc1ac74e7269c568816574ea7c0a6f7ae678c3
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-06-01 18:14:20 +1000

    can link to top level api folders; others expand; moved mermain js into the repo

--- source/assets/javascripts/accordion.js
+++ source/assets/javascripts/accordion.js
@@ -3,14 +3,12 @@ $('a.collapsed').each(function(index, element) {
   $("." + key).slideToggle('fast');
 });
 
-$('a.collapsed').bind('click', function(e) {
+var toggleList = function(e) {
   var key = $(this).data('key');
   $("." + key).slideToggle('fast');
-  e.preventDefault();
-});
 
-$('a.expanded').bind('click', function(e) {
-  var key = $(this).data('key');
-  $("." + key).slideToggle('fast');
   e.preventDefault();
-});
+};
+
+$('a.collapsed').bind('click', toggleList);
+$('a.expanded').bind('click', toggleList);
